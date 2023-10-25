1a87a68e6c7fb42bc487ceaab3947ca52ba445a3
Whywehere <104337697+whywehere@users.noreply.github.com>
2023-10-25 20:08:54 +0800

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"Interpreter/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
+
+func main() {
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		u.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
